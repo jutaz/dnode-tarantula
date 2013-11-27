bded8df0c903572ffed2c88f8538263f5581d295
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-11-27 15:41:24 +0200

    Move ./lib/index.js to ./index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./lib/index');
+var server = require('./lib/Server');
+var client = require('./lib/Client');
+
+module.exports = {
+	Server: server,
+	Spider:	server,
+	Tarantula: server,
+	server: server,
+	Client:	client,
+	client: client,
+	Fly: client,
+};
