9e01143880537da9977ad1422e6321d1467c56b7
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-11-30 16:05:46 +0200

    Make redis store prototype much better

---
+++ stores/redis.js
@@ -0,0 +1,41 @@
+function store() {
+
+}
+
+store.prototype.publish = function(data, callback) {
+
+}
+
+store.prototype.subscribe = function(name, callaback) {
+
+}
+
+store.prototype.unsubscribe = function(name, callback) {
+
+}
+
+store.prototype.destroy = function(callback) {
+
+}
+
+store.prototype.set = function(client, callback) {
+
+}
+
+store.prototype.get = function(id, callback) {
+
+}
+
+store.prototype.delete = function(id, callback) {
+
+}
+
+store.prototype.ids = function(callback) {
+
+}
+
+store.prototype.changeId = function(id, newId, callback) {
+
+}
+
+module.exports = store;
