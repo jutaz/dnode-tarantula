41a6feee27139caad1e1ae6f02d54927955f3b2d
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-11-30 13:41:06 +0200

    TABS TO SPACES

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var server 		= require('./lib/Server');
-var client 		= require('./lib/Client');
+var server      = require('./lib/Server');
+var client      = require('./lib/Client');
 var memoryStore = require('./stores/memory');
 
 module.exports = {
-	Server: 	server,
-	Spider:		server,
-	Tarantula: 	server,
-	server: 	server,
-	Client:		client,
-	client: 	client,
-	Fly: 		client,
-	stores: 	{
-		memory:	memoryStore
-	},
+    Server:     server,
+    Spider:     server,
+    Tarantula:  server,
+    server:     server,
+    Client:     client,
+    client:     client,
+    Fly:        client,
+    stores:     {
+        memory: memoryStore
+    },
 };
--- stores/memory.js
+++ stores/memory.js
@@ -1,41 +1,41 @@
 function memory() {
-	this.clients = {};
+    this.clients = {};
 }
 
 memory.prototype.set = function(client, callback) {
-	this.clients[client.id] = client;
-	(callback && callback(null, client))
+    this.clients[client.id] = client;
+    (callback && callback(null, client))
 }
 
 memory.prototype.get = function(id, callback) {
-	client = this.clients[id];
-	if(!client) {
-		callback(null, false);
-	} else {
-		callback(null, client);
-	}
+    client = this.clients[id];
+    if(!client) {
+        callback(null, false);
+    } else {
+        callback(null, client);
+    }
 }
 
 memory.prototype.delete = function(id, callback) {
-	this.clients[id] = null;
-	delete this.clients[id];
-	(callback && callback(null))
+    this.clients[id] = null;
+    delete this.clients[id];
+    (callback && callback(null))
 }
 
 memory.prototype.ids = function(callback) {
-	callback(null, Object.keys(this.clients))
+    callback(null, Object.keys(this.clients))
 }
 
 memory.prototype.changeId = function(id, newId, callback) {
-	var self = this;
-	this.get(id, function(err, client) {
-		client.id = newId;
-		self.set(client, function(err, client) {
-			self.delete(id, function(err) {
-				callback(err);
-			});
-		})
-	});
+    var self = this;
+    this.get(id, function(err, client) {
+        client.id = newId;
+        self.set(client, function(err, client) {
+            self.delete(id, function(err) {
+                callback(err);
+            });
+        })
+    });
 }
 
 module.exports = memory;
