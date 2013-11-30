25035b6b979e6ac3380b4a82eafc3b086b026a8b
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-11-30 12:16:10 +0200

    Add memory store prototype

---
+++ stores/memory.js
@@ -0,0 +1,17 @@
+function memory() {
+	this.clients = {};
+}
+
+memory.prototype.set = function(client, callback) {
+
+}
+
+memory.prototype.get = function(id, callback) {
+
+}
+
+memory.prototype.delete = function(id, callack) {
+
+}
+
+module.exports = memory;
